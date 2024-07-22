99001db820d38e757167be4f7681f76eabf50480
mpinchover <mpinchover@gmail.com>
2024-07-21 20:33:11 -0700

    added in better backgrounds

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -1,6 +1,11 @@
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import { Text, TextInput } from "react-native-paper";
-import { Button, Divider } from "react-native-paper";
+import { Divider, Button as RNPButton } from "react-native-paper";
 import {
   useFonts,
   Inter_900Black,
@@ -13,52 +18,81 @@ const Login = ({ navigation }) => {
   });
 
   return (
-    <View style={styles.container}>
-      <Text variant="displayLarge" style={styles.title}>
-        Slept
-      </Text>
-      <TextInput
-        label="Email"
-        style={styles.textInput}
-        // value={text}
-        // onChangeText={(text) => setText(text)}
-      />
-      <Divider style={{ margin: 4 }} />
-      <TextInput
-        label="Password"
-        style={styles.textInput}
+    <ImageBackground
+      source={require("../../assets/backgrounds/bg_4_darkened.png")} // Replace with your image URL
+      resizeMode="cover" // This prop ensures the image covers the whole area
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text variant="displayLarge" style={styles.title}>
+          Slept
+        </Text>
+        <TextInput
+          label="Email"
+          style={styles.textInput}
+          // value={text}
+          // onChangeText={(text) => setText(text)}
+        />
+        <Divider style={{ margin: 4 }} />
+        <TextInput
+          label="Password"
+          style={styles.textInput}
 
-        // value={text}
-        // onChangeText={(text) => setText(text)}
-      />
-      <Button
-        textColor="#fffbff"
-        style={{
-          borderWidth: 1,
-          width: "100%",
-          color: "#fffbff",
-        }}
-      >
-        Forgot password?
-      </Button>
-      <Divider style={{ margin: 4 }} />
+          // value={text}
+          // onChangeText={(text) => setText(text)}
+        />
 
-      <Button style={{ width: "100%", margin: 0 }} mode="contained">
-        Log in
-      </Button>
+        <Divider style={{ margin: 8 }} />
+        <RNPButton style={{ width: "100%", margin: 0 }} mode="contained">
+          Log in
+        </RNPButton>
 
-      <Button
-        onPress={() => navigation.navigate("Signup")}
-        textColor="#fffbff"
-        style={{
-          borderWidth: 1,
-          width: "100%",
-          color: "#fffbff",
-        }}
-      >
-        Don't have an account? Sign up.
-      </Button>
-    </View>
+        <TouchableOpacity
+          color="#fffbff"
+          style={{
+            width: "100%",
+            color: "#fffbff",
+            marginTop: 10,
+
+            // borderWidth: 1,
+            // borderColor: "black",
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "#fffbff",
+              fontSize: 16,
+            }}
+          >
+            Forgot Password?
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Signup")}
+          color="#fffbff"
+          style={{
+            width: "100%",
+            color: "#fffbff",
+            marginTop: 10,
+
+            // borderWidth: 1,
+            // borderColor: "black",
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "#fffbff",
+              fontSize: 16,
+            }}
+          >
+            Don't have an account? Sign up.
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -69,9 +103,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#6700D3",
-    position: "relative",
     paddingHorizontal: 20,
+    width: "100%",
+    position: "relative",
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    // paddingHorizontal: 20,
+
+    left: 0,
+    right: 0,
+    position: "relative",
+    // paddingHorizontal: 20,
   },
   title: {
     color: "#fffbff",
--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -1,65 +1,107 @@
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import { Text, TextInput } from "react-native-paper";
-import { Button, Divider } from "react-native-paper";
+import { Divider, Button as RNPButton } from "react-native-paper";
 import {
   useFonts,
   Inter_900Black,
   Inter_100Thin,
   Inter_200ExtraLight,
 } from "@expo-google-fonts/inter";
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
+const supabaseUrl = "https://wdylwouqozgzhdnqkrnx.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+// const supabase = createClient(
+//   supabaseUrl,
+// );
+
 const Signup = ({ navigation }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   let [fontsLoaded] = useFonts({
     Inter_200ExtraLight,
-    Inter_900Black,
   });
 
+  const signUpUser = async () => {
+    setIsLoading(true);
+    try {
+      // const { data, error } = await supabase.auth.signUp({
+      //   email: "example@email.com",
+      //   password: "example-password",
+      // });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text variant="displayLarge" style={styles.title}>
-        Slept
-      </Text>
-      <TextInput
-        label="Name"
-        style={styles.textInput}
-        // value={text}
-        // onChangeText={(text) => setText(text)}
-      />
-      <Divider style={{ margin: 4 }} />
-      <TextInput
-        label="Email"
-        style={styles.textInput}
-        // value={text}
-        // onChangeText={(text) => setText(text)}
-      />
-      <Divider style={{ margin: 4 }} />
-      <TextInput
-        label="Password"
-        style={styles.textInput}
+    <ImageBackground
+      source={require("../../assets/backgrounds/bg_5_darkened.png")} // Replace with your image URL
+      resizeMode="cover" // This prop ensures the image covers the whole area
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text variant="displayLarge" style={styles.title}>
+          Slept
+        </Text>
+        <TextInput
+          label="Name"
+          style={styles.textInput}
+          // value={text}
+          // onChangeText={(text) => setText(text)}
+        />
+        <Divider style={{ margin: 4 }} />
+        <TextInput
+          label="Email"
+          style={styles.textInput}
+          // value={text}
+          // onChangeText={(text) => setText(text)}
+        />
+        <Divider style={{ margin: 4 }} />
+        <TextInput
+          label="Password"
+          style={styles.textInput}
 
-        // value={text}
-        // onChangeText={(text) => setText(text)}
-      />
+          // value={text}
+          // onChangeText={(text) => setText(text)}
+        />
 
-      <Divider style={{ margin: 8 }} />
-      <Button
-        style={{ width: "100%", margin: 0 }}
-        mode="contained"
-        onPress={() => console.log("Pressed")}
-      >
-        Sign up
-      </Button>
+        <Divider style={{ margin: 8 }} />
+        <RNPButton
+          style={{ width: "100%", margin: 0, fontSize: 12 }}
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          Sign up
+        </RNPButton>
 
-      <Button
-        onPress={() => navigation.navigate("Login")}
-        textColor="#fffbff"
-        style={{
-          width: "100%",
-          color: "#fffbff",
-        }}
-      >
-        Have an account? Log in.
-      </Button>
-    </View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Login")}
+          style={{
+            width: "100%",
+            color: "#fffbff",
+            marginTop: 10,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "#fffbff",
+              fontSize: 16,
+            }}
+          >
+            Have an account? Log in.
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -70,9 +112,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#6700D3",
-    position: "relative",
     paddingHorizontal: 20,
+    width: "100%",
+    position: "relative",
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    // paddingHorizontal: 20,
+
+    left: 0,
+    right: 0,
+    position: "relative",
+    // paddingHorizontal: 20,
   },
   title: {
     color: "#fffbff",
